ed69177c6c06a69dc44415e492371d837bb42395
MT0TH <38347101+T0thM@users.noreply.github.com>
2019-04-02 10:47:23 +0200

    Add files via upload

---
+++ silly_openssl_decrypter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -e -p "Please enter the encrypted file path: " enc_file
+read -e -p "Please enter wordlist location: " wordvar
+CIPHER="aes-256-cbc" 
+
+# Openssl encrypted, base64 encoded files brute-forcer/decrypter
+# Using aes-256-cbc cipher, but you are free to modify this"
+# Silly solution, but working (tested on CTF machine)
+# Need to get rid of CRLF line endings with dos2unix
+
+for i in $(cat $wordwar | dos2unix); do echo -e "\nTesting password: $i"; openssl $CIPHER -d -a -in $enc_file -pass pass:$i | grep -i -E '[A-Za-z0-9]{3,30}';done < password.txt 2>/dev/null | grep -v -i -E 'testing|binary' | grep -v "^$"
+
+exit 0
