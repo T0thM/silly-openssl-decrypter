cfa4c4fdaeb4527cda296fd9d29302bba4555ac5
MT0TH <38347101+T0thM@users.noreply.github.com>
2019-04-02 10:48:16 +0200

    Update silly_openssl_decrypter.sh

--- silly_openssl_decrypter.sh
+++ silly_openssl_decrypter.sh
@@ -7,7 +7,7 @@ CIPHER="aes-256-cbc"
 # Openssl encrypted, base64 encoded files brute-forcer/decrypter
 # Using aes-256-cbc cipher, but you are free to modify this"
 # Silly solution, but working (tested on CTF machine)
-# Need to get rid of CRLF line endings with dos2unix
+# Needed to get rid of CRLF line endings with dos2unix
 
 for i in $(cat $wordwar | dos2unix); do echo -e "\nTesting password: $i"; openssl $CIPHER -d -a -in $enc_file -pass pass:$i | grep -i -E '[A-Za-z0-9]{3,30}';done < password.txt 2>/dev/null | grep -v -i -E 'testing|binary' | grep -v "^$"
 
